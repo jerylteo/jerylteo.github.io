65a4ba3692e4a5a56b0099af3da752af05e05ef8
Jeryl-HOME <jerylteo1337@gmail.com>
2019-10-08 21:44:52 +0800

    revert

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer>
+        <!-- <div class="left">
+            <a href="#">Portfolio</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </div> -->
+        <div class="mid">
+            <p>Jeryl Teo &copy; Copyright 2019</p>
+        </div>
+    </footer>
+</body>
+</html>
