529f7959cb26e8112796dae33201f1b3dc673b2e
Jeryl-HOME <jerylteo1337@gmail.com>
2019-10-08 21:36:10 +0800

    Test
    global header

--- index.php
+++ index.php
@@ -1,33 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="Portfolio Webpage featuring Jeryl Teo">
-    <meta name="author" content="Jeryl Teo">
-    <title>Portfolio - Jeryl Teo</title>
-    <link rel="stylesheet" href="css/index.css">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto&display=swap" rel="stylesheet">
-
-</head>
-<body>
-    <header>
-        <div class="container">
-            <div id="leftNav">
-                <a href="index.html" id="highlight">JERYL TEO</a>
-                <p>Cyber Security ● Digital Media ● Programming ● Photography</p>
-            </div>
-            <nav>
-                <ul>
-                    <li class="current"><a href="portfolio.html">Portfolio</a></li>
-                    <li><a href="#gallery">Gallery</a></li>
-                    <li><a href="#aboutMe">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-        
-    </header>
+<?php
+include 'header.php';
+?>
     <section id="showcase">
         <div id="cont">
             <div id="showcaseText">
