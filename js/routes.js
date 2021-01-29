657977a6a35a019277241fb1ec1fa9213343ea9d
Jeryl <jerylteo@outlook.com>
2021-01-29 17:24:18 +0800

    fix routing err

--- js/routes.js
+++ js/routes.js
@@ -1,11 +1,4 @@
 const mainComp = {template: '<app-main></app-main>'};
-// const landingComp = {template: '<app-landing></app-landing>'};
-// const websiteComp = {template: '<app-website></app-website>'};
-// const swimmingComp = {template: '<app-swimming></app-swimming>'};
-// const photographyComp = {template: '<app-photography></app-photography>'};
-// const aboutComp = {template: '<app-about></app-about>'};
-// const contactComp = {template: '<app-contact></app-contact>'};
-
 const routes = [
     {path: '/', component: mainComp},
 ];
